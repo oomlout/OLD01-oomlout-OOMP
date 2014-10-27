ae725f408caa6ca4cdc76e9036f86e7ae1789b16
oomlout <aaron@oomlout.com>
2014-10-27 07:16:00 +0000

    Minor Changes

--- OOMP-generateSingle.bat
+++ OOMP-generateSingle.bat
@@ -1,7 +1,7 @@
 @echo off
 set TPGEdirectory=C:\GH\oomlout-TPGE\
 set WIKBdirectory=C:\GH\oomlout-WIKB\
-set id=CRHO-I01-B-02PI-01
+set id=IDCS-I01-X-2X10-
 
 
 REM
@@ -9,17 +9,17 @@ REM Generate Part Page
 REM
 
 	REM      Generate Page
-REM python %TPGEdirectory%TPGEmain.py -id %id% -bd C:\KB\oomp-scripts\oomp-gen\parts\%id%\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-template.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%id%\%id%-wiki.html
+python %TPGEdirectory%TPGEmain.py -id %id% -bd C:\KB\oomp-scripts\oomp-gen\parts\%id%\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-template.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%id%\%id%-wiki.html
 
 	REM Upload Page
-REM python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\KB\oomp-scripts\oomp-gen\parts\%id%\%id%-wiki.html -wb projects/oomp/part/%id% 
+python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\KB\oomp-scripts\oomp-gen\parts\%id%\%id%-wiki.html -wb projects/oomp/part/%id% 
 
 REM
 REM Generate Info Page
 REM
 set extra=familyIndex
 	REM      Generate Page
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html
+REM python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html
 	
 	REM Upload Page	
-python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html -wb projects/oomp/%extra%
+REM python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html -wb projects/oomp/%extra%
