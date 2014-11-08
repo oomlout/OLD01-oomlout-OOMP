35cd77091ba133d40e5e938537437bc576d4a9cb
oomlout <aaron@oomlout.com>
2014-11-08 20:29:46 +0000

    adding part files for first time

--- OOMP-generateSingle.bat
+++ OOMP-generateSingle.bat
@@ -10,7 +10,7 @@ REM Generate Part Page
 REM
 
 	REM      Generate Page
-REM python %TPGEdirectory%TPGEmain.py -id %id% -bd %OOBBpartsDirectory%%id%\ -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-template.tmpl.html -of %OOBBpartsDirectory%wiki/%id%\%id%-wiki.html
+REM python %TPGEdirectory%TPGEmain.py -id %id% -bd %OOBBpartsDirectory%%id%\ -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-template.tmpl.html -of %OOBBpartsDirectory%%id%\%id%-wiki.html
 
 	REM Upload Page
 REM python %WIKBdirectory%WIKBmain.py -si oomlout -bd %OOBBpartsDirectory%%id%\%id%-wiki.html -wb projects/oomp/part/%id% 
@@ -20,7 +20,7 @@ REM Generate Info Page
 REM
 set extra=completenessIndex
 	REM      Generate Page
-python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of %OOBBpartsDirectory%wiki/%extra%-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex C:\GH\oomlout-OOMP\OOMP-helpers.oomp -tm template/OOMP-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
 	
 	REM Upload Page	
 python %WIKBdirectory%WIKBmain.py -si oomlout -bd %OOBBpartsDirectory%%extra%-wiki.html -wb projects/oomp/%extra%
