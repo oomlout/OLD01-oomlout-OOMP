58301bd5275d2555c170e2a27894724ec5e15237
oomlout <aaron@oomlout.com>
2014-11-08 18:03:21 +0000

    Added gitignore

--- OOMP-generate.bat
+++ OOMP-generate.bat
@@ -2,12 +2,14 @@
 set TPGEdirectory=C:\GH\oomlout-TPGE\
 
 
+set OOBBpartsDirectory=C:\GH\oomlout-OOMP\parts\
+
 
 REM
 REM     Generating all part pages
 REM
 
-python %TPGEdirectory%TPGEmain.py -rm A -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-template.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%%%%ID%%%%\%%%%ID%%%%-wiki.html
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
 
 
 
@@ -18,12 +20,14 @@ REM
 
 set extra=allParts
 	REM All Parts
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
 	
 	REM PictureIndex
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-pictureIndex.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\pictureIndex-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-pictureIndex.tmpl.html -of %OOBBpartsDirectory%pictureIndex-wiki.html
 	REM FamilyIndex
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-familyIndex.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\familyIndex-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-familyIndex.tmpl.html -of %OOBBpartsDirectory%familyIndex-wiki.html
+	REM COmpletness Test
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-completenessIndex.tmpl.html -of %OOBBpartsDirectory%completenessIndex-wiki.html
 
 
 REM
@@ -33,17 +37,17 @@ REM
 
 set extra=idDescribed
 	REM All Parts
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
 	
 set extra=idCodes
 	REM All Parts
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
 	
 set extra=tagsDescribed
 	REM All Parts
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
 	
 set extra=resources
 	REM All Parts
-python %TPGEdirectory%TPGEmain.py -bd C:\KB\oomp-scripts\oomp-gen\parts\ -xa .oomp -ex C:\KB\oomp-scripts\oomp-gen\parts\,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of C:\KB\oomp-scripts\oomp-gen\parts\%extra%-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\ -tm template/OOMP-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
 		
