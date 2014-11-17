c1eb613f9fc10bd77b36e9a5966e23a162226d08
oomlout <aaron@oomlout.com>
2014-11-17 21:53:31 +0000

    Regenerated WIki, Added a few parts

--- OOMP-createNew.bat
+++ OOMP-createNew.bat
@@ -5,7 +5,7 @@ set OOBBpartsDirectory=C:\GH\oomlout-OOMP\parts\
 
 set extra=newItem
 	REM All Parts
-python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\OOMP-%extra%.Source.oomp -tm template/OOMP-%extra%.tmpl.oomp -of %OOBBpartsDirectory%AAAA!!0,newOOMP.oompType,oompType!!-!!0,newOOMP.oompSize,oompSize!!-!!0,newOOMP.oompColor,oompColor!!-!!0,newOOMP.oompDesc,oompDesc!!-!!0,newOOMP.oompIndex,oompIndex!!\!!0,newOOMP.oompType,oompType!!-!!0,newOOMP.oompSize,oompSize!!-!!0,newOOMP.oompColor,oompColor!!-!!0,newOOMP.oompDesc,oompDesc!!-!!0,newOOMP.oompIndex,oompIndex!!.oomp
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOMP\OOMP-%extra%.Source.oomp -tm template/OOMP-%extra%.tmpl.oomp -of %OOBBpartsDirectory%!!0,newOOMP.oompType,oompType!!-!!0,newOOMP.oompSize,oompSize!!-!!0,newOOMP.oompColor,oompColor!!-!!0,newOOMP.oompDesc,oompDesc!!-!!0,newOOMP.oompIndex,oompIndex!!\!!0,newOOMP.oompType,oompType!!-!!0,newOOMP.oompSize,oompSize!!-!!0,newOOMP.oompColor,oompColor!!-!!0,newOOMP.oompDesc,oompDesc!!-!!0,newOOMP.oompIndex,oompIndex!!.oomp
 	
 
 
