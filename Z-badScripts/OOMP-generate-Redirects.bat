0e151bf6bc887e2620e28e7ff4d5eaf6125cfc76
oomlout <aaron@oomlout.com>
2019-06-05 17:16:38 +0100

    Fixed some template issues.

--- Z-badScripts/OOMP-generate-Redirects.bat
+++ Z-badScripts/OOMP-generate-Redirects.bat
@@ -1,9 +1,23 @@
+@echo on
+set TPGEdirectory=C:\GH\oomlout-TPGE\
+set REDIdirectory=C:\GH\oomlout-OOMP\redirects\
+
+set OOBBpartsDirectory=C:\GH\oomlout-OOMP\parts\
+set OOMPwikiDirectory=C:\GH\oomlout-OOMP.wiki\
+set OOMPwikiDirectoryParts=C:\GH\oomlout-OOMP.wiki\parts\
+set OOMPprojectsDirectory=C:\GH\oomlout-OOMP\projects\
+set OOMPDirectory=C:\GH\oomlout-OOMP\
+set INKSdirectory=C:\GH\oomlout-INKS\
+
 REM 
 REM 	Redirect Pages
 REM 
 
-REM 111 python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory% -xa .oomp -ex C:\GH\oomlout-OOMP\OOMP-helpers.oomp -tm %OOMPdirectory%template/OOMP-Redirect-template.tmpl.html -of %REDIdirectory%%%%%ID%%%%\index.html
-REM 111 python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory% -xa .oomp -ex C:\GH\oomlout-OOMP\OOMP-helpers.oomp -tm %OOMPdirectory%template/OOMP-Redirect-template.tmpl.html -of %REDIdirectory%@@%%%%ID%%%%,oompPart.oompID,hexID@@\index.html
+
+
+
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory% -xa .oomp -ex C:\GH\oomlout-OOMP\OOMP-helpers.oomp -tm %OOMPdirectory%template/OOMP-Redirect-template.tmpl.html -of %REDIdirectory%%%%%ID%%%%\index.html
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory% -xa .oomp -ex C:\GH\oomlout-OOMP\OOMP-helpers.oomp -tm %OOMPdirectory%template/OOMP-Redirect-template.tmpl.html -of %REDIdirectory%@@%%%%ID%%%%,oompPart.oompID,hexID@@\index.html
 
 REM 
 REM 	Redirect Pages Datasheet
